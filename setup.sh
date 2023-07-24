2520aec3e2acb5094d8a02697bd9004172ee79ab
abhi8h <140423082+abhi8h@users.noreply.github.com>
2023-07-24 20:43:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-vikrantsingh8709@gmail.com\"\n\
+email = \"tousif62059494@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
